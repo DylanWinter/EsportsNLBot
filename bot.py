2ee9ea0949dc25cd9d15c5dc12738188646441a5
Dylan Winter <dlwinter@mun.ca>
2025-07-12 17:33:32 -0230

    cleanup

--- bot.py
+++ bot.py
@@ -35,18 +35,19 @@ async def on_message(message):
     if message.content.startswith("-") and bot.active_veto is not None:
         try:
             map_to_ban = message.content[1:]
-            bot.active_veto.ban(map_to_ban, int(message.author.id))
+            if bot.active_veto.can_user_ban(int(message.author.id)):
+                bot.active_veto.ban(map_to_ban, int(message.author.id))
 
-            if bot.active_veto.completed:
-                maps_text = display_list(bot.active_veto.maps_remaining)
-                bot.active_veto = None
-                await message.channel.send("Banned map " + map_to_ban.capitalize() +
-                                           "\nMap(s) for the match: " + maps_text)
-            else:
-                mentions = " ".join(f"<@{user_id}>" for user_id in bot.active_veto.active_team)
-                await message.channel.send(f"Banned map {map_to_ban.capitalize()}"+
-                                           f"\nTeam banning: {mentions}"
-                                           f"\nMaps remaining: {display_list(bot.active_veto.maps_remaining)}")
+                if bot.active_veto.completed:
+                    maps_text = display_list(bot.active_veto.maps_remaining)
+                    bot.active_veto = None
+                    await message.channel.send("Banned map " + map_to_ban.capitalize() +
+                                               "\nMap(s) for the match: " + maps_text)
+                else:
+                    mentions = " ".join(f"<@{user_id}>" for user_id in bot.active_veto.active_team)
+                    await message.channel.send(f"Banned map {map_to_ban.capitalize()}"+
+                                               f"\nTeam banning: {mentions}"
+                                               f"\nMaps remaining: {display_list(bot.active_veto.maps_remaining)}")
         except ValueError as e:
             await message.channel.send(str(e))
 
@@ -59,11 +60,13 @@ async def map_pool(interaction: discord.Interaction):
 
 @bot.tree.command(name="startveto", description="Starts a veto for the specified number of maps (default 1)", guild=discord.Object(id=guild_id))
 async def start_veto(interaction: discord.Interaction, team1: str, team2: str, num_maps: int = 1):
+    if num_maps != 1 and num_maps != 3 and num_maps != 5:
+        await interaction.response.send_message("Invalid veto. Supply 1, 3 or 5 maps.")
     with open("config.json", "r") as f:
         data = json.load(f)
     maps = data["maps"]
     if bot.active_veto is not None:
-        await interaction.response.send_message("There is already an active veto")
+        await interaction.response.send_message("There is already an active veto.")
     bot.active_veto = Veto(maps, parse_users(team1), parse_users(team2), num_maps)
     mentions = " ".join(f"<@{user_id}>" for user_id in bot.active_veto.active_team)
     await interaction.response.send_message(f"**Starting Veto With:** {display_list(maps)}" +
--- veto.py
+++ veto.py
@@ -12,12 +12,6 @@ class Veto:
 
     """ Bans a map, updates the active team """
     def ban(self, map_to_ban: str, user_id: int):
-        print(user_id, self.active_team)
-        print("active_team:", self.active_team, [type(u) for u in self.active_team])
-        print(f"Check: {user_id} in {self.active_team} = {user_id in self.active_team}")
-        print(id(self.team1[0]), id(self.active_team[0]))
-        if int(user_id) not in self.active_team:
-            raise ValueError("User not in banning team.")
         if map_to_ban.lower() in self.maps_remaining:
             self.maps_remaining.remove(map_to_ban.lower())
             self.banned_maps.append(map_to_ban.lower())
@@ -33,3 +27,6 @@ class Veto:
             raise ValueError("Map already banned: "+ map_to_ban)
         else:
             raise ValueError("Map not in maps list: " + map_to_ban)
+
+    def can_user_ban(self, user_id: int):
+        return int(user_id) in self.active_team
